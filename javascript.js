b8d9a10bfc261a61761802a6cac9d6e4455f160f
w1883045 <w1883045@my.westminster.ac.uk>
2024-12-27 17:48:33 +0000

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -2,3 +2,23 @@ function toggleNav() {
     var nav = document.getElementById("navbar");
     nav.classList.toggle("active");
 }
+
+//get button
+let backToTopButton = document.getElementById('backToTop');
+
+//show button when user scrolls down
+window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
+
+//scroll to top when clicked
+backToTopButton.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+};
