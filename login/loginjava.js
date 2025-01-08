76f4c551a6af640d2b6c6583228e06e121c6a0c9
w1883045 <w1883045@my.westminster.ac.uk>
2025-01-08 00:03:09 +0000

    Add files via upload

--- login/loginjava.js
+++ login/loginjava.js
@@ -16,4 +16,9 @@ document.addEventListener("DOMContentLoaded", () => {
             alert("Invalid login credentials. Please Try Again.");
         }
     })
-})
+});
+
+function toggleNav() {
+    var nav = document.getElementById("navbar");
+    nav.classList.toggle("active");
+}
