c5f9d5ca826d464e621aa8e4c7cfb18e66ab86aa
w1883045 <w1883045@my.westminster.ac.uk>
2025-01-11 01:23:59 +0000

    Add files via upload

--- login/loginjava.js
+++ login/loginjava.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.getElementById("loginForm");
     const usernameField = document.getElementById("username");
+    const passwordField = document.getElementById("password");
+    const togglePassword = document.getElementById("togglePassword");
 
     //handle form submission
     loginForm.addEventListener("submit", (e) => {
@@ -8,14 +10,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const username = usernameField.value;
 
-        if (username) {
+        if (username && passwordField.value) {
             localStorage.setItem("loggedInUser", username);
 
             window.location.href = "/eLife/profile/profile.html";
         } else {
             alert("Invalid login credentials. Please Try Again.");
         }
-    })
+    });
+
+    //Toggle password visibility
+    togglePassword.addEventListener("click", () => {
+        const isPasswordHidden = passwordField.getAttribute("type") === "password";
+        passwordField.setAttribute("type", isPasswordHidden ? "text" : "password");
+        togglePassword.innerHTML = isPasswordHidden
+            ? '<i class="fa-solid fa-eye-slash"></i>'
+            : '<i class="fa-solid fa-eye"></i>';
+    });
 });
 
 function toggleNav() {
